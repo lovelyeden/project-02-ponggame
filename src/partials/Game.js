10250eeb3552f77e10dcea6514d6c8eb72436d43
lovely <ldde07@gmail.com>
2019-04-25 14:08:04 -0700

    update on Board filename

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,5 +1,8 @@
 import { SVG_NS } from "../settings";
 
+// this line is new
+import Board from './Board';
+
 
 export default class Game {
   constructor(element, width, height) {
@@ -9,19 +12,22 @@ export default class Game {
 
     this.gameElement = document.getElementById(this.element);
 
+    this.board = new Board(this.width, this.height);
+
 		// Other code goes here...
   }
 
   render() {
     // More code goes here....
+    this.gameElement.innerHTML= '';
+
     let svg = document.createElementNS(SVG_NS, "svg");
     svg.setAttributeNS(null, "width", this.width);
     svg.setAttributeNS(null, "height", this.height);
     svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
 
-
-    this.gameElement.innerHTML= '';
+    this.board.render(svg);
 
   }
 }
